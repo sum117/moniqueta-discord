57213969e38b8533309f498f656d016cdc26eebb
sum117 <jvcaliman@hotmail.com>
2022-07-03 19:52:44 -0300

    t

--- src/commands/prefixless/messageCounter.js
+++ src/commands/prefixless/messageCounter.js
@@ -4,7 +4,7 @@ module.exports = {
   name: "Contador de Mensagens",
   description: "Automaticamente conta as mensagens de cada jogador.",
   async execute(msg) {
-    const currentValue = await db.get(msg.author.id);
+    const currentValue = await db.get(`msgTop_${msg.author.id}`);
     db.set(`msgTop_${msg.author.id}`, currentValue + 1);
   },
 };
