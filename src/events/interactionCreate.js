46137dea5e9a346c318eb33b4a6fe2c9fb6f9551
sum117 <jvcaliman@hotmail.com>
2022-07-03 20:19:15 -0300

    t

--- src/events/interactionCreate.js
+++
@@ -1,9 +0,0 @@
-const PlaycardPlayer = require("../structures/SDA/PlayCardPlayer");
-const Discord = require("discord.js");
-module.exports = {
-  name: "interactionCreate",
-  /**@param {Discord.Interaction} interaction A interação que ativou o evento */
-  async execute(interaction) {
-    interaction.reply('Ainda não configurado...')
-  },
-};
