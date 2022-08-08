048975191cbd0f5b7d6241ccc3cdf47eabdad778
sum117 <jvcaliman@hotmail.com>
2022-08-08 04:11:14 -0300

    forgot async declarations

--- src/commands/prefix/top.js
+++ src/commands/prefix/top.js
@@ -13,33 +13,32 @@ export const data = {
  * @param {String} args Os argumentos enviados pelo bot para a execução do comando.
  */
 export async function execute(msg) {
+  const msgTop = await db.table('msgTop').all();
+  let firstPlace;
+  const str = await msgTop
+    .sort((entryBefore, entryAfter) => entryAfter.value - entryBefore.value)
+    .map(async (entry, index) => {
+      switch (index) {
+        case 0:
+          const fetchMember = async user => await msg?.guild.members.fetch({user: user.id});
+          firstPlace = await fetchMember(entry.id, msg);
+          return `👑 ${userMention(entry.id)} - ${entry.value}`;
+        case 1:
+          return `🥈 ${userMention(entry.id)} - ${entry.value}`;
+        case 2:
+          return `🥉 ${userMention(entry.id)} - ${entry.value}`;
+        default:
+          return `${index + 1}. ${userMention(entry.id)} - ${entry.value}`;
+      }
+    })
+    .splice(0, 15)
+    .join('\n');
 
-    const msgTop = await db.table('msgTop').all();
-    let firstPlace;
-    const str = msgTop
-        .sort((entryBefore, entryAfter) => entryAfter.value - entryBefore.value)
-        .map((entry, index) => {
-            switch (index) {
-                case 0:
-                    const fetchMember = async user => await msg?.guild.members.fetch({user: user.id});
-                    firstPlace = await fetchMember(entry.id, msg);
-                    return `👑 ${userMention(entry.id)} - ${entry.value}`;
-                case 1:
-                    return `🥈 ${userMention(entry.id)} - ${entry.value}`;
-                case 2:
-                    return `🥉 ${userMention(entry.id)} - ${entry.value}`;
-                default:
-                    return `${index + 1}. ${userMention(entry.id)} - ${entry.value}`;
-            }
-        })
-        .splice(0, 15)
-        .join('\n');
-
-    const embed = new MessageEmbed()
-        .setTitle('🏆 MENSAGENS - TOP 10 🏆')
-        .setDescription(str)
-        .setColor('RANDOM')
-        .setTimestamp(Date.now())
-        .setThumbnail(firstPlace.user.displayAvatarURL({ dynamic: true, size: 512 }));
-    await msg.reply({ embeds: [embed] });
+  const embed = new MessageEmbed()
+    .setTitle('🏆 MENSAGENS - TOP 10 🏆')
+    .setDescription(str)
+    .setColor('RANDOM')
+    .setTimestamp(Date.now())
+    .setThumbnail(firstPlace.user.displayAvatarURL({dynamic: true, size: 512}));
+  await msg.reply({embeds: [embed]});
 }
