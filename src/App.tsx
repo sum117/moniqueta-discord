ab900f3643fd7890c1bdcbb744257e51f3aa48b6
sum117 <jvcaliman@hotmail.com>
2022-08-14 04:14:55 -0300

    in react.ts

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { InputBoxes } from './components/InputBoxes';
+import styles from './App.module.css';
+export function App() {
+  return (
+    <div>
+      <h1 className={styles.header}>TRADUTOR DE SUBTRATO</h1>
+      <InputBoxes></InputBoxes>
+    </div>
+  );
+}
