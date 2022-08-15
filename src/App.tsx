7c7d5605fb044d5778adf284f86f3e64f2d382a1
sum117 <75037449+sum117@users.noreply.github.com>
2022-08-15 04:24:53 +0000

    Trying to fix auto zoom in react ap

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styles from './App.module.css';
 export function App() {
   return (
     <div>
-      <h1 className={styles.header}>TRADUTOR DE SUBTRATO</h1>
+      <h1 className={styles.header}>TRADUTOR DE SUBTRATO</h1>  
       <InputBoxes></InputBoxes>
     </div>
   );
