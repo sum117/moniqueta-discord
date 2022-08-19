c1be7967840915fe62adb58364f8000f0c47fa10
sum117 <jvcaliman@hotmail.com>
2022-08-19 03:11:19 -0300

    mudaeTimer

---
+++ src/commands/cron/index.js
@@ -0,0 +1,1 @@
+export * as mudaeTimer from './mudaeTimer.js';
---
+++ src/commands/cron/mudaeTimer.js
@@ -0,0 +1,22 @@
+import {CronJob} from 'cron';
+
+export const data = {
+  name: 'Mudae Roll Timer',
+  type: 'cron',
+  description: 'Atualiza um canal do servidor com um temporizador da mudae.'
+};
+
+export function execute(client, timer, channel, minutes, role) {
+  timer = client.channels.cache.get(timer);
+  channel = client.channels.cache.get(channel);
+  const update = async () => {
+    const isGreen = timer.name.startsWith('🟢');
+    await timer.edit({
+      name: isGreen ? '🔴 Mudae...' : '🟢 Mudae!'
+    });
+    if (!isGreen) await channel.send(`${role ? `❤️ Hora da mudae, <@&${role}>!` : '❤️ Hora da mudae, pessoal!'}`);
+    return isGreen;
+  };
+
+  return new CronJob('* 33,40 * * * *', update(), null, true, 'America/Recife');
+}
