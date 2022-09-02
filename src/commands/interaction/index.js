f98144356e92438c8aeaf9a8fd8b2733e9ffaa39
sum117 <jvcaliman@hotmail.com>
2022-09-02 16:02:27 -0300

    added new prefix command system and ran eslint, also organized folders

--- src/commands/interaction/index.js
+++ src/commands/interaction/index.js
@@ -1,4 +1,4 @@
 export * as sheet from './ficha.js';
 export * as interact from './interact.js';
-export * as roleSelector from './selectRoles.js';
 export * as sdcPremiados from './sdcPremiados.js';
+export * as roleSelector from './selectRoles.js';
