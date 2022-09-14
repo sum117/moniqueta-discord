90d43549adfb802fad1fcae99a00d670ee02e915
sum117 <jvcaliman@hotmail.com>
2022-09-14 12:09:28 -0300

    added new isAdmin guard

--- src/guards/index.ts
+++ src/guards/index.ts
@@ -1,3 +1,4 @@
+export { isAdmin } from './isAdmin.js';
 export { isNotBot } from './isNotBot.js';
 export { isAllowedParent } from './isAllowedParent.js';
 export { hasCharacter } from './hasCharacter.js';
