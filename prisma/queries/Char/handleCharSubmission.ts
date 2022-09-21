b87a50f3215c5bf7b6604020f4ecd767ec50033e
sum117 <jvcaliman@hotmail.com>
2022-09-21 11:01:39 -0300

    added essential prisma queries

---
+++ prisma/queries/Char/handleCharSubmission.ts
@@ -0,0 +1,33 @@
+import {PrismaClient} from "@prisma/client";
+import {Snowflake} from "discord.js";
+
+const prisma = new PrismaClient();
+
+export class handleCharSubmission {
+
+    public static async create(authorId: string, charId: number, messageIdBundle: Snowflake[]) {
+        return prisma.charSubmission.create({
+            data: {
+                authorId: authorId,
+                messageIdBundle: messageIdBundle.join(','),
+                charId: charId
+            }
+        })
+    }
+
+    public static async delete(charId: number) {
+        return prisma.charSubmission.deleteMany({
+            where: {
+                charId: charId
+            }
+        })
+    }
+
+    public static async get(charId: number) {
+        return prisma.charSubmission.findFirst({
+            where: {
+                charId: charId
+            }
+        })
+    }
+}
