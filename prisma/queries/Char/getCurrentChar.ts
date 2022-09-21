d3dcca80a521384db79c3e087f2cd864aeddf74c
sum117 <jvcaliman@hotmail.com>
2022-09-21 11:02:59 -0300

    changed query name and organized rest with folder

--- prisma/queries/Char/getCurrentChar.ts
+++ prisma/queries/Char/getCurrentChar.ts
@@ -4,7 +4,7 @@ import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function getChar(arg: Message | CommandInteraction) {
+export async function getCurrentChar(arg: Message | CommandInteraction) {
   return prisma.char.findUnique({
     where: {
       chosenById:
