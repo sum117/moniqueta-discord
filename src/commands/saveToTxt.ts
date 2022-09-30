bd980723eba6fcd13eeaadf2b548ff9df846bd56
sum117 <jvcaliman@hotmail.com>
2022-09-30 05:33:13 -0300

    added new Util Command

---
+++ src/commands/saveToTxt.ts
@@ -0,0 +1,59 @@
+import {
+  ApplicationCommandOptionType,
+  Collection,
+  CommandInteraction,
+  Snowflake,
+} from 'discord.js';
+import { Discord, Slash, SlashOption } from 'discordx';
+import { Util } from '../util/Util';
+
+@Discord()
+export class Tools {
+  @Slash({
+    name: 'salvar',
+    description: 'Salva as mensagens do canal em um arquivo txt',
+  })
+  async saveToTxt(
+    @SlashOption({
+      name: 'limite',
+      type: ApplicationCommandOptionType.Number,
+      description: 'Limite de mensagens a serem salvas',
+      required: false,
+    })
+    limit: number = 1000,
+    @SlashOption({
+      name: 'depois_da_mensagem',
+      type: ApplicationCommandOptionType.String,
+      description: 'ID da mensagem a partir da qual as mensagens serão salvas',
+      required: false,
+    })
+    after: Snowflake,
+    interaction: CommandInteraction,
+  ) {
+    const messages = await Util.monsterFetch(interaction, { before: '', after, limit });
+    if (messages instanceof Collection) {
+      const embeds = messages
+        .reverse()
+        .map((message) => message.embeds?.[0])
+        .filter((embed) => embed ?? false);
+
+      const indentText = (text: string) =>
+        text
+          .split('\n')
+          .map((line) => `    ${line}`)
+          .join('\n');
+      const content = embeds
+        .map((embed) => indentText(embed.description ?? ''))
+        .join('\n');
+      const buffer = Buffer.from(content);
+      await interaction.editReply({
+        files: [
+          {
+            attachment: buffer,
+            name: 'mensagens.txt',
+          },
+        ],
+      });
+    }
+  }
+}
--- src/util/Util.ts
+++ src/util/Util.ts
@@ -3,18 +3,25 @@ import {
   Message,
   BaseMessageOptions,
   userMention,
-} from "discord.js";
+  CommandInteraction,
+  Collection,
+} from 'discord.js';
 
+interface FetchOptions {
+  before?: string;
+  after?: string;
+  limit: number;
+}
 export class Util {
   public static handleAttachment(
     message: Message,
     reply: BaseMessageOptions,
-    embed: EmbedBuilder
+    embed: EmbedBuilder,
   ) {
     const attachment = message.attachments.first();
     if (attachment) {
-      const attachmentName = attachment.url.split("/").pop();
-      embed.setImage("attachment://" + attachmentName);
+      const attachmentName = attachment.url.split('/').pop();
+      embed.setImage('attachment://' + attachmentName);
       reply.embeds = [embed];
       reply.files = [
         {
@@ -25,9 +32,9 @@ export class Util {
     }
     if (message.mentions.users) {
       const mentions = message.mentions.users.map((user) =>
-        userMention(user.id)
+        userMention(user.id),
       );
-      reply.content = mentions.join(",");
+      reply.content = mentions.join(',');
     }
   }
 
@@ -37,8 +44,88 @@ export class Util {
   public static titleCase = (str: string): string => {
     return str
       .toLowerCase()
-      .split(" ")
+      .split(' ')
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
+      .join(' ');
   };
+  public static async monsterFetch(
+    interaction: CommandInteraction,
+    options: FetchOptions,
+  ) {
+    if (!interaction.deferred || !interaction.replied)
+      await interaction.deferReply();
+
+
+    const errorMessages = {
+      invalidInteraction: 'O objeto de interação não é válido.',
+      invalidPositionalParam: 'O parâmetro "before" ou "after" não é válido.',
+      fetchingError: 'Ocorreu um erro ao buscar as mensagens ou editar a mensagem de carregamento: ',
+      noMessagesInChannel: 'Não há mensagens neste canal.',
+    };
+
+    const loadingText = 'Carregando mensagens...\n';
+    const loadedText = 'Mensagens carregadas.';
+    if (!(interaction instanceof CommandInteraction))
+      return console.log(errorMessages.invalidInteraction);
+
+    // Check if the options provided are indeed messages that exist in the channel.
+
+    if (options.after) {
+      try {
+        await interaction.channel?.messages.fetch({ ...options, limit: 2 });
+      } catch {
+        return interaction.editReply(errorMessages.invalidPositionalParam);
+      }
+    } else {
+      if (!interaction.channel?.lastMessageId) return interaction.editReply(errorMessages.noMessagesInChannel);
+      options.before = interaction.channel.lastMessageId;
+    }
+
+    const delay = (s: number) =>
+      new Promise((resolve) => setTimeout(resolve, s * 1000));
+    const loadingMessage = await interaction.channel?.send(loadingText);
+    if (!loadingMessage) return;
+    const fetchWithMode = (option: 'top/bottom' | 'bottom/top'): Promise<Collection<string,Message>> =>  {
+      const quocient =
+        Math.ceil(options.limit / 100) < 1 ? 1 : Math.ceil(options.limit / 100);
+      let messages: Collection<string, Message> = new Collection();
+      return new Promise(async (resolve, reject) => {
+        if (!interaction.channel?.messages) return;
+        for (let i = 0; i < quocient; i++) {
+          try {
+            await delay(5);
+            const fetchedMessages = await interaction.channel?.messages.fetch({
+              ...options,
+              limit: 100,
+            });
+            messages = messages.concat(messages, fetchedMessages);
+
+            const positionalOption =
+              option === 'top/bottom' ? 'after' : 'before';
+
+            if (positionalOption === 'after')
+              options[positionalOption] = fetchedMessages.reverse().last()?.id ?? '';
+            else options[positionalOption] = fetchedMessages.last()?.id ?? '';
+            if (options[positionalOption] === '') {
+              resolve(messages);
+              loadingMessage.edit(loadedText);
+              break;
+            };
+
+            await loadingMessage.edit(`${loadingText} ${i + 1}/${quocient}`);
+            if (i === quocient - 1) {
+              resolve(messages)
+              loadingMessage.edit(loadedText);
+            };
+          } catch (err) {
+            reject(console.log(errorMessages.fetchingError + err));
+            break;
+          }
+        }
+      });
+    };
+
+    if (options.after) return fetchWithMode('top/bottom');
+    return fetchWithMode('bottom/top');
+  }
 }
