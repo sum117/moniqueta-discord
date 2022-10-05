1b1f8a2b3e39ecfb34a57a8922d4f577018e7dd5
Ubuntu <jcaliman@web-dev.ifjnjg5y04kurbvhtwtki3xd3h.nx.internal.cloudapp.net>
2022-10-05 06:41:33 +0000

    Users can now roleplay inside threads

--- src/guards/isAllowedParent.ts
+++ src/guards/isAllowedParent.ts
@@ -1,7 +1,7 @@
-import {CommandInteraction, Snowflake, TextChannel} from 'discord.js';
-import {ArgsOf, GuardFunction} from 'discordx';
+import { CommandInteraction, Snowflake, TextChannel, ThreadChannel } from 'discord.js';
+import { ArgsOf, GuardFunction } from 'discordx';
 
-import {ErrorMessage} from '../util/ErrorMessage';
+import { ErrorMessage } from '../util/ErrorMessage';
 
 export function isAllowedParent(channels: Snowflake[]) {
   const guard: GuardFunction<ArgsOf<'messageCreate'> | CommandInteraction> = async (
@@ -11,7 +11,14 @@ export function isAllowedParent(channels: Snowflake[]) {
   ) => {
     const argObj = arg instanceof Array ? arg[0] : arg;
     const channel = argObj.channel;
-    if (!(channel instanceof TextChannel) || channel.isDMBased()) return;
+    if (channel instanceof ThreadChannel) {
+      // Check if ThreadChannel belongs to channel in allowedParents array
+      const hostChannel = channel.parent;
+      // if it belongs return pass otherwise void
+      if (hostChannel?.isTextBased() && channels.includes(hostChannel?.parentId ?? '')) return next();
+      return;
+    }
+    if (!channel?.isTextBased() || channel.isDMBased()) return;
     if (channels.includes(channel.parentId ?? '')) {
       await next();
     } else {
